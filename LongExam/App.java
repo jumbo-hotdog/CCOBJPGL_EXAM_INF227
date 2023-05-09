ce637c7223b0de430bfcc52c6e344ef1d92f0e65
Joshua Guimbaolibot <119920263+jumbo-hotdog@users.noreply.github.com>
2023-05-09 11:54:43 +0800

    Update App.java

--- LongExam/App.java
+++ LongExam/App.java
@@ -30,14 +30,5 @@ public class App {
         tagaytay.accept(me);
         System.out.println();
 
-        // Long exam 1
-
-        // 1. Add 5 new locations to visit
-        // 2. Each location should have an airFare.
-        // 3. Each time you visit a new location, your budget should decrease.
-        // 4. In the visit method, print a message if you can visit the new location and
-        // print another message if you cannot visit the location.
-        // 5. Each time you visit a location, call the checkBudgetMethod
-
     }
 }
